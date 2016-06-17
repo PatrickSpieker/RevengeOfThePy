8a94ab8bb7660fe464897585e01af9292271faf5
Patrick Spieker <pspieker@cs.washington.edu>
2016-06-17 02:03:55 -0700

    Finished springer hybrid scraper

--- src/csv_to_sql.py
+++ src/csv_to_sql.py
@@ -4,5 +4,5 @@ paths_to_data = []
 for path in paths_to_data:
     with open(path, 'r') as f:
         reader = csv.reader()
-        next(reader)  # eliminating column names
+        next(reader) # eliminating column names
         # do SQL stuff
